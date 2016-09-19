91ea27dbda02ff3805b3dea00eeae480a985e8db
Alexey Ermakov <ermakov.as@mipt.ru>
2016-09-19 17:00:25 +0300

    Add task 3-3

---
+++ pyrob/solutions/task_3_3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+from pyrob.api import *
+
+
+@task
+def task_3_3():
+
+    if not wall_is_on_the_right():
+        move_right()
+    elif not wall_is_on_the_left():
+        move_left()
+    elif not wall_is_above():
+        move_up()
+    else:
+        move_down()
+
+run_tasks()
