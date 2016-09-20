d7e5d6bd102be877eff1cd20ae5b103092bfa9ba
Alexey Ermakov <ermakov.as@mipt.ru>
2016-09-20 21:08:08 +0300

    Script to create images with mazes for all tasks

--- pyrob/solutions/all_tasks.py
+++ pyrob/solutions/all_tasks.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
 
 import sys
-from pyrob.api import *
+from pyrob.api import run_tasks
 
-import task_1_1
-import task_1_2
-import task_2_1
-import task_2_2
-import task_2_4
-import task_3_1
-import task_3_3
-import task_5_2
-import task_5_3
-import task_5_4
-import task_5_7
-import task_8_21
-import task_8_22
-import task_8_27
-import task_8_28
-import task_8_29
-import task_4_3
-import task_4_11
-import task_5_10
-import task_6_6
-import task_8_30
-import task_9_3
-import task_7_5
-import task_7_6
-import task_7_7
-import task_8_18
-import task_8_2
-import task_8_3
-import task_8_4
-import task_8_6
-import task_8_10
-import task_8_11
+from pyrob.solutions import task_1_1
+from pyrob.solutions import task_1_2
+from pyrob.solutions import task_2_1
+from pyrob.solutions import task_2_2
+from pyrob.solutions import task_2_4
+from pyrob.solutions import task_3_1
+from pyrob.solutions import task_3_3
+from pyrob.solutions import task_5_2
+from pyrob.solutions import task_5_3
+from pyrob.solutions import task_5_4
+from pyrob.solutions import task_5_7
+from pyrob.solutions import task_8_21
+from pyrob.solutions import task_8_22
+from pyrob.solutions import task_8_27
+from pyrob.solutions import task_8_28
+from pyrob.solutions import task_8_29
+from pyrob.solutions import task_4_3
+from pyrob.solutions import task_4_11
+from pyrob.solutions import task_5_10
+from pyrob.solutions import task_6_6
+from pyrob.solutions import task_8_30
+from pyrob.solutions import task_9_3
+from pyrob.solutions import task_7_5
+from pyrob.solutions import task_7_6
+from pyrob.solutions import task_7_7
+from pyrob.solutions import task_8_18
+from pyrob.solutions import task_8_2
+from pyrob.solutions import task_8_3
+from pyrob.solutions import task_8_4
+from pyrob.solutions import task_8_6
+from pyrob.solutions import task_8_10
+from pyrob.solutions import task_8_11
 
 
 if __name__ == '__main__':
